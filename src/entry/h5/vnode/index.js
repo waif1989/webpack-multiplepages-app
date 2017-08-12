343db9d3afd663ba0e260885b5488a5d9fb6081a
ccw <1094123350@qq.com>
2017-08-12 18:35:36 +0800

    rewrite analysisTag function

--- src/entry/h5/vnode/index.js
+++ src/entry/h5/vnode/index.js
@@ -3,13 +3,11 @@
  */
 import { createVnode } from '../../../utils/create-vnode'
 import { createRealnode } from '../../../utils/create-realnode'
-import { analysisHtml, arrToVnode } from '../../../utils/template-analysis'
+import { analysisHtml } from '../../../utils/template-analysis'
 import tem from './template.html'
 const results = analysisHtml(tem)
-// arrToVnode(results)
-const a = arrToVnode(results)
-console.log('----', a)
+console.log('results---index-----', results)
 const vdom = createVnode('ul', {'class': 'list'}, createVnode('li', {}, 'item1'), createVnode('li', {}, 'item2'))
-console.log('+++++', vdom)
+console.log('vdom-----', vdom)
 const app = document.getElementById('app')
 app.appendChild(createRealnode(vdom))
--- src/utils/template-analysis.js
+++ src/utils/template-analysis.js
@@ -1,20 +1,103 @@
-var HTML = require('html-parse-stringify')
+// var HTML = require('html-parse-stringify')
 var createVnode = require('./create-vnode').createVnode
-var arrtem = []
-function analysisHtml (tem) {
-    var results = HTML.parse(tem)
-    return results
-}
-function arrToVnode (arr) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i].type === 'tag') {
-            console.log('tag-------', arr[i])
-            arrtem.push(createVnode(arr[i].name, arr[i].attrs))
-            arrToVnode(arr[i].children)
-        } else {
-            console.log('content-------', arr[i])
-        }
+
+function analysisTag (tag) {
+    var attrRegexp = /([\w-]+)|['"]{1}([^'"]*)['"]{1}/g
+    var selfClosingTags = {
+        base: true,
+        br: true,
+        col: true,
+        embed: true,
+        hr: true,
+        img: true,
+        input: true,
+        keygen: true,
+        link: true,
+        menuitem: true,
+        meta: true,
+        param: true,
+        source: true,
+        track: true,
+        wbr: true
     }
-    return arrtem
+    var i = 0
+    var key
+    var res = {
+        type: 'tag',
+        name: '',
+        selfClosing: false,
+        attrs: {},
+        children: []
+    }
+    tag.replace(attrRegexp, function (match) {
+        if (i % 2) {
+            key = match     // Check odd number target item. And match this target's attruties name
+        } else {
+            if (i === 0) {
+                // In the first target, Check this target is self-closing or not
+                if (selfClosingTags[match] || tag.charAt(tag.length - 2) === '/') {
+                    res.selfClosing = true
+                }
+                res.name = match
+            } else {
+                res.attrs[key] = match.replace(/['"]/g, '')  // "res.attrs" is an object inside. This will setup "attrs" object name and object key
+            }
+        }
+        i++
+    })
+    return res
 }
-module.exports = { analysisHtml, arrToVnode }
+function analysisHtml (html) {
+    // var results = HTML.parse(tem)
+    // return results
+    console.log('htmlstring-------', html)
+    var tagRegexp = /<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g
+    var result = []
+    var arr = []
+    var targetResult
+    var level = -1
+    var inComponent = false
+    var options = {}
+    options.components = {}
+    html.replace(tagRegexp, function (match, index) {
+        var isTargetStart = match.charAt(1) !== '/'  // Judge this target is end or just start
+        var nextstart = index + match.length         // Calculate the next string after this target's start position
+        var nextChar = html.charAt(nextstart)        // Get this target's following char string
+        var parent
+        console.log('tag-------', match, index, isTargetStart, nextstart, nextChar)
+        if (isTargetStart) {
+            level++
+            targetResult = analysisTag(match)
+            if (targetResult.type === 'tag' && options.components[targetResult.name]) {
+                targetResult.type = 'component'
+                inComponent = true
+            }
+            if (!targetResult.selfClosing && !inComponent && nextChar && nextChar !== '<') {
+                targetResult.children.push({
+                    type: 'text',
+                    content: html.slice(nextstart, html.indexOf('<', nextstart))
+                })
+            }
+            if (level === 0) {
+                result.push(targetResult)
+            }
+            parent = arr[level - 1]
+            if (parent) {
+                parent.children.push(targetResult)
+            }
+            arr[level] = targetResult
+        }
+        if (!isTargetStart || targetResult.selfClosing) {
+            level--
+            if (!inComponent && nextChar !== '<' && nextChar) {
+                // trailing text node
+                arr[level].children.push({
+                    type: 'text',
+                    content: html.slice(start, html.indexOf('<', start))
+                });
+            }
+        }
+    })
+    return result
+}
+module.exports = { analysisHtml }
