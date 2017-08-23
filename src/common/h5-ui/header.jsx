b694afc9718bf7706c7c00f67fa52a0dc03ab21d
chensiwei <chensiwei@corp.netease.com>
2017-08-23 18:27:13 +0800

    Encapsulate the UI component v0.0.1

--- src/common/h5-ui/header.jsx
+++ src/common/h5-ui/header.jsx
@@ -1,37 +1,64 @@
 import React, { Component } from 'react'
-import { render, findDOMNode } from 'react-dom'
+import { render } from 'react-dom'
 import ReactDOMServer from 'react-dom/server'
 
+let text1Style = {
+    color: 'blue'
+}
+let text2Style = {
+    color: 'red'
+}
+
+let d1Style = {
+    width: '200px',
+    height: '200px',
+    backgroundColor: '#EEE'
+}
+
+let initProps = 4
 
 class Header extends Component {
     constructor (props) {
         super(props)
     }
     componentDidMount () {
-        const el = findDOMNode(this)
-        console.log('====', el.outerHTML)
+        // console.log('--------//', this.props.someProperty)
     }
     render () {
         return (
-            <div className="d1">
-                <p className="text1">This is JSX Header</p>
+            <div className="d1" id="d1" style={d1Style}>
+                {this.props.someProperty > 1 ? <p style={text1Style} id="text">text1</p> : <p style={text2Style} id="text">text2</p>}
             </div>
         )
     }
 }
 
-/*const Header = (
-    <div className="d1">
-        <p className="text1">heeel</p>
-    </div>
-)*/
-
-function header () {
-   /* render(
-        <Header />,
-        document.getElementById('app')
-    )*/
-    return ReactDOMServer.renderToString(<Header />)
+function header (options /* @param options {Object} */) {
+    /* @param options
+     * -- @property styleCustom {Object}
+     * -- @property componProps {Object}
+     * -- @property reactRender {Boolean}
+     * */
+    if (!options) {
+        return ReactDOMServer.renderToStaticMarkup(<Header someProperty={initProps} />)
+    } else {
+        if (options.styleCustom) {
+            d1Style = options.styleCustom.d1Style ? Object.assign({}, d1Style, options.styleCustom.d1Style) : d1Style
+            text1Style = options.styleCustom.text1Style ? Object.assign({}, text1Style, options.styleCustom.text1Style) : text1Style
+            text2Style = options.styleCustom.text2Style ? Object.assign({}, text2Style, options.styleCustom.text2Style) : text2Style
+        }
+        if (options.componProps) {
+            initProps = options.componProps.initProps ? options.componProps.initProps : initProps
+        }
+        if (options.reactRender) {
+            render(
+                <Header someProperty={initProps} />,
+                options.rootDocId ? document.getElementById(options.rootDocId) : document.getElementById('app')
+            )
+        }
+        return ReactDOMServer.renderToStaticMarkup(<Header someProperty={initProps} />)
+    }
 }
+
 
 export default header
--- src/entry/h5/jsx/index.js
+++ src/entry/h5/jsx/index.js
@@ -2,8 +2,22 @@
  * Created by chensiwei on 2017/7/27.
  */
 
-// import { header } from '../../../common/h5-ui/header'
-// const { header } = require('../../../common/h5-ui/header')
 import header from '../../../common/h5-ui/header'
 // header()
-console.log('-----***', header())
+const h = header({
+    reactRender: false,
+    styleCustom: {
+        d1Style: {
+            fontSize: '30px',
+            backgroundColor: 'yellow'
+        }
+    },
+    componProps: {
+        initProps: 1
+    }
+})
+document.getElementById('app').innerHTML = h
+document.getElementById('text').addEventListener('click', () => {
+    alert('998')
+})
+// console.log('-----***', header())
